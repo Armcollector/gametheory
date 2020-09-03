ff4bf50200138c5e284729a144040cfbfcef3a4d
Tommy <10076072+tommyod@users.noreply.github.com>
2020-09-03 19:00:26 +0200

    Simple solver for 1P GET

--- get1000/main.py
+++ get1000/main.py
@@ -14,6 +14,8 @@ import copy
 import tabulate
 import random
 import sys
+import operator
+import time
 
 sys.setrecursionlimit(2 ** 30)
 
@@ -54,6 +56,9 @@ class Get:
         new.board[i][j] = value
         return new
 
+    def __hash__(self):
+        return hash(tuple(tuple(r) for r in self.board))
+
     def num_free_positions(self):
         return len(list(self.free_positions()))
 
@@ -77,6 +82,13 @@ class Get:
                 if entry is None:
                     yield (i, j)
                     break  # Go to the next column
+
+    def __eq__(self, other):
+        for attr in ["target", "shape", "board"]:
+            if getattr(self, attr) != getattr(other, attr):
+                return False
+
+        return True
 
     def __getitem__(self, index):
         i, j = index
@@ -180,3 +192,91 @@ if __name__ == "__main__":
 
     entries = get.shape[0] * get.shape[0]
     print(f"States of nature: {len(get.digits) ** entries}")
+
+
+if __name__ == "__main__":
+
+    # =============================================================================
+    # ============ THE STRATEGY IS FIXED, TRY EVERY POSSIBLE DICE ROLL ============
+    # =============================================================================
+
+    import functools
+
+    @functools.lru_cache(maxsize=2 ** 26)
+    def get_best_placement(get, digit, ell=1, level=0):
+        """Returns (index, optimal_score).
+
+        ell = expected l norm used in computation
+                E[|x_i|**(ell)]**(1/ell)
+
+
+
+        """
+
+        free_positions = list(get.free_positions())
+
+        # CASE: Bottom of recursion - there is only one placement available
+        if len(free_positions) == 1:
+            index = free_positions[0]
+            new_get = get.set_item(index, digit)
+            return index, new_get.score()
+
+        # CASE: There are many placements available, we'll try each of them
+
+        results = {}
+        for index in get.canonical_free_positions():
+
+            # Set this digit in the position
+            new_get = get.set_item(index, digit)
+
+            # How many digits should we try on recursion?
+            # TODO: Better logic here, or try everything
+
+            digits_try_to = random.choices(new_get.digits, k=max(3 - level, 1))
+
+            prob = 1 / len(digits_try_to)
+
+            indices_scores = list(
+                get_best_placement(new_get, digit, ell=ell, level=level + 1) for digit in digits_try_to
+            )
+            scores = [score for (index, score) in indices_scores]
+
+            # Here we take E[|x_i|**(ell)]**(1/ell) - an expected value l norm
+            # The case ell = 1 corresponds to the expected value
+            # As ell -> infty this corresponds to minimizing the maximum deviation from the target
+            # Should be computed in log space for better numerical stability
+            results[index] = (sum(abs(s) ** ell * prob for s in scores)) ** (1 / ell)
+
+        return min(results.items(), key=operator.itemgetter(1))
+
+    # Create some random games
+    for seed in range(10):
+
+        st = time.time()
+
+        random.seed(seed)
+        get = Get.get1000()  # Get.get1000()
+        nature = [random.choice(get.digits) for _ in range(get.shape[0] * get.shape[1])]
+
+        for digit in nature:
+            opt_index, opt_score = get_best_placement(get, digit, ell=1)
+            # print(f"Got digit {digit}. Placing it in {index} for expected score: {opt_score}")
+            get = get.set_item(opt_index, digit)
+
+            # print(get)
+
+        print(f"Final score: {get.score()}")
+
+        get = Get.get1000()  # Get.get1000()
+        for digit in nature:
+
+            opt_index, opt_score = get_best_placement(get, digit, ell=2)
+            # print(f"Got digit {digit}. Placing it in {index} for expected score: {opt_score}")
+            get = get.set_item(opt_index, digit)
+
+            # print(get)
+
+        print(f"Final score: {get.score()}")
+        print(f"Time spent: {time.time() - st}")
+
+        print()
