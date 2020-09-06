732b10ee6fbd1ce3ed42169f87ac973d5b012569
Helge Hatteland <helge.hatteland@sonat.no>
2020-09-06 14:56:01 +0200

    Solve exercise 6.9a

--- gametheorygame.py
+++ gametheorygame.py
@@ -1,27 +1,19 @@
-from itertools import combinations_with_replacement, product
-
+from itertools import permutations
 
 class Game:
     
-    def __init__(self):
-        self.strategies = ['A','B']
-        self.utility = {('A','A') : [-2,-2], ('A','B'): [-10,-1], ('B','A') : [-1,-10], ('B','B'): [-5,-5]}
+    def __init__(self, utility):
+        self.utility = dict(utility)
+        self.strategies = sorted({ strategy
+            for outcome in utility.keys()
+            for strategy in outcome
+        })
 
     def u(self, player, p_strat, opp_strat):
         if player == 0:
             return self.utility[p_strat, opp_strat][player]
         else:
             return self.utility[opp_strat, p_strat][player]
-
-    def set_utility(self, utilities):
-        """
-            list of 4 list of 2 representing (A,A), (A,B), (B,A), (B,B)
-        """
-        
-        self.utility[('A','A')] = utilities[0]
-        self.utility[('A','B')] = utilities[1]
-        self.utility[('B','A')] = utilities[2]
-        self.utility[('B','B')] = utilities[3]
 
     def is_strongly_dominated(self, player, s_i, s_mark):
         return all(self.u(player,s_mark, s_opp) > self.u(player, s_i,s_opp)  for s_opp in self.strategies)
@@ -54,7 +46,7 @@ class Game:
 
     def pure_nash(self):
         nash_eq = []
-        for a,b in product(self.strategies,repeat=2):
+        for a,b in self.utility.keys():
             #test for player 0
             p0 = all(self.u(0,a, b) >= self.u(0,s_i, b) for s_i in self.strategies)
             p1 = all(self.u(1,b, a) >= self.u(1,s_i, a) for s_i in self.strategies)
@@ -64,7 +56,7 @@ class Game:
     
     def strongly_dominant_equilibrium(self):
         strong_eq = []
-        for a,b in product(self.strategies,repeat=2):
+        for a,b in self.utility.keys():
             #test for player 0
             p0 = self.is_strongly_dominant_strategy(0,a)
             p1 = self.is_strongly_dominant_strategy(1,b)
@@ -75,7 +67,7 @@ class Game:
 
     def weakly_dominant_equilibrium(self):
         weakly_eq = []
-        for a,b in product(self.strategies,repeat=2):
+        for a,b in self.utility.keys():
             #test for player 0
             p0 = self.is_weakly_dominant_strategy(0,a)
             p1 = self.is_weakly_dominant_strategy(1,b)
@@ -86,7 +78,7 @@ class Game:
 
     def very_weakly_dominant_equilibrium(self):
         very_weakly_eq = []
-        for a,b in product(self.strategies,repeat=2):
+        for a,b in self.utility.keys():
             #test for player 0
             p0 = self.is_very_weakly_dominant_strategy(0,a)
             p1 = self.is_very_weakly_dominant_strategy(1,b)
@@ -94,6 +86,12 @@ class Game:
             if p0 and p1:
                 very_weakly_eq.append((a,b))
         return very_weakly_eq
+
+    def print_equilibrium(self):
+        print(f"Strongly dominant equilibrium: {self.strongly_dominant_equilibrium()}")
+        print(f"Weakly dominant equilibrium: {self.weakly_dominant_equilibrium()}")
+        print(f"Very weakly dominant equilibrium: {self.very_weakly_dominant_equilibrium()}")
+        print(f"Nash equilibrium: {self.pure_nash()}")
 
     def maxmin_value(self,player):
         return max( min(  self.u(player, p_strat, opp_strat) for opp_strat in self.strategies )  for p_strat in self.strategies )
@@ -112,7 +110,10 @@ class Game:
         return [opp_strat for opp_strat in self.strategies if max(self.u(player, p_strat, opp_strat) for p_strat in self.strategies) == v ]
 
 def tests():
-    g = Game()
+    g = Game({
+        ('A','A'): [-2,-2], ('A','B'): [-10,-1],
+        ('B','A'): [-1,-10], ('B','B'): [-5,-5],
+    })
    
     assert g.is_strongly_dominated(0,'A','B')
     assert g.is_strongly_dominated(1,'A','B')
@@ -124,7 +125,10 @@ def tests():
     assert g.has_strongly_dominant_strategy(0)
     assert g.has_strongly_dominant_strategy(1)
 
-    g.set_utility([[-2,-2],[-10,-2],[-2,-10],[-5,-5]])
+    g = Game({
+        ('A','A'): [-2,-2], ('A','B'): [-10,-2],
+        ('B','A'): [-2,-10], ('B','B'): [-5,-5],
+    })
 
     assert not g.has_strongly_dominant_strategy(0)
     assert not g.has_strongly_dominant_strategy(1)
@@ -136,34 +140,52 @@ def tests():
     assert g.weakly_dominant_equilibrium() == [('B','B')]
 
     #example 6.1
-    g.set_utility([[2,1],[0,0],[0,0],[1,2]])
+    g = Game({
+        ('A','A'): [2,1], ('A','B'): [0,0],
+        ('B','A'): [0,0], ('B','B'): [1,2],
+    })
     assert g.pure_nash() == [('A','A'),('B','B')]
 
     assert not g.strongly_dominant_equilibrium()
 
     #example 6.2
-    g.set_utility([[-2,-2],[-10,-1],[-1,-10],[-5,-5]])
+    g = Game({
+        ('A','A'): [-2,-2], ('A','B'): [-10,-1],
+        ('B','A'): [-1,-10], ('B','B'): [-5,-5],
+    })
     assert g.pure_nash() == [('B','B')]
 
-    g.set_utility([[6,6],[0,8],[8,0],[3,3]])
+    g = Game({
+        ('A','A'): [6,6], ('A','B'): [0,8],
+        ('B','A'): [8,0], ('B','B'): [3,3],
+    })
     assert g.pure_nash() == [('B','B')]
 
     #maxmin
-    g.set_utility([[4,1],[0,4],[1,5],[1,2]])
+    g = Game({
+        ('A','A'): [4,1], ('A','B'): [0,4],
+        ('B','A'): [1,5], ('B','B'): [1,2],
+    })
     assert g.maxmin_value(0) == 1
     assert g.maxmin_value(1) == 2
 
-    g.set_utility([[4,1],[0,4],[1,5],[1,1]])
+    g = Game({
+        ('A','A'): [4,1], ('A','B'): [0,4],
+        ('B','A'): [1,5], ('B','B'): [1,1],
+    })
     assert g.maxmin_strategies(0) == ['B']
     assert g.maxmin_strategies(1) == ['A','B']
-    
+
 
 def exercise_6_9():
     #exercise 6.9.a)
-    g = Game()
-    for comb in combinations_with_replacement(range(1,8),8):
-        g.set_utility([comb[i: i+2] for i in range(0, len(comb), 2)])
+    outcomes = [('A', 'A'), ('A', 'B'), ('B', 'A'), ('B', 'B')]
+    games = lambda: (
+        Game({ k:v for k,v in zip(outcomes, zip(comb[::2], comb[1::2]))})
+        for comb in permutations([0,1]*8,8)
+    )
 
+    for g in games():
         if len(g.pure_nash()) == 1:
             if g.pure_nash()[0] not in g.weakly_dominant_equilibrium():
                 print(g.utility, " has unique nash that is not weakly dominant.")
@@ -173,9 +195,7 @@ def exercise_6_9():
         
 
     #exercise 6.9.b)
-    for comb in combinations_with_replacement(range(1,9),8):
-        g.set_utility([comb[i: i+2] for i in range(0, len(comb), 2)])
-
+    for g in games():
         if len(g.pure_nash()) == 1:
             if g.pure_nash()[0] in g.weakly_dominant_equilibrium() and g.pure_nash()[0] not in g.strongly_dominant_equilibrium():
                 print(g.utility, " has unique nash that is weakly dominant, but not strongly dominant")
@@ -184,9 +204,7 @@ def exercise_6_9():
         print("could not find solution.")
 
     #exercise 6.9.c)
-    for comb in combinations_with_replacement(range(1,9),8):
-        g.set_utility([comb[i: i+2] for i in range(0, len(comb), 2)])
-
+    for g in games():
         if g.weakly_dominant_equilibrium() and g.pure_nash():
             if any( i not in  g.weakly_dominant_equilibrium() and i not in g.weakly_dominant_equilibrium() for i in g.pure_nash()):
                 print(g.utility, " has nash and weak ")
@@ -195,9 +213,10 @@ def exercise_6_9():
         print("could not find solution.")
 
 def exercise_6_3():
-    g = Game()
-
-    g.set_utility([[0,1],[1,1],[1,1],[1,0]])
+    g = Game({
+        ('A','A'): [0,1], ('A','B'): [1,1],
+        ('B','A'): [1,1], ('B','B'): [1,0],
+    })
 
     print(f"pure nash in : {g.pure_nash()}")
     for p in [0,1]:
