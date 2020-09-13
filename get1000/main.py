d06516d263e77f93493c319ab0bf93ea8c713d42
tommyod <tommy.odland@gmail.com>
2020-09-13 20:37:53 +0200

    refactored latex doc

--- get1000/main.py
+++ get1000/main.py
@@ -16,6 +16,7 @@ import random
 import sys
 import operator
 import time
+import statistics
 
 sys.setrecursionlimit(2 ** 30)
 
@@ -46,15 +47,34 @@ class Get:
         new = type(self)(shape=self.shape, target=self.target, digits=self.digits)
         new.board = copy.deepcopy(self.board)
         return new
+    
+    def expected_score(self):
+        new_get = self.copy()
+        expected_value = statistics.mean(new_get.digits)
+        for index in new_get.free_positions():
+            new_get = new_get.set_item(index, expected_value)
+        return new_get.score()
 
     def set_item(self, index, value):
         """Set an item and return a copy."""
         new = self.copy()
         i, j = index
-        assert value in self.digits
+        assert min(self.digits) <= value <= max(self.digits)
         assert new.board[i][j] is None
         new.board[i][j] = value
         return new
+    
+    def set_item_at_column(self, column_j, value):
+        """Set an item and return a copy."""
+        new = self.copy()
+        assert min(self.digits) <= value <= max(self.digits)
+        
+        for i, row in enumerate(self.board):
+            for j, entry in enumerate(row):
+                if j == column_j and entry is None:
+                    return new.set_item((i, j), value)
+                
+        assert False, "No entry in column was free"
 
     def __hash__(self):
         return hash(tuple(tuple(r) for r in self.board))
@@ -89,6 +109,12 @@ class Get:
                 return False
 
         return True
+    
+    def to_latex(self):
+        out = r'\begin{bmatrix}'
+        out += r'\\'.join(' & '.join(self.EMPTY_ENTRY_CHAR if e is None else str(e) for e in row) for row in self.board)
+        out += r'\end{bmatrix}'
+        return out
 
     def __getitem__(self, index):
         i, j = index
@@ -109,8 +135,11 @@ class Get:
         if list(self.free_positions()):
             return float("inf")
         else:
-            row_values = (int("".join(str(e) for e in r)) for r in self.board)
-            return sum(row_values) - self.target
+            score = 0
+            for row in self.board:
+                for j, entry in enumerate(reversed(row), 0):
+                    score += entry * 10**j
+            return score - self.target
 
 
 if __name__ == "__main__":
@@ -212,6 +241,10 @@ if __name__ == "__main__":
 
 
         """
+        if level > 3:
+            return None, get.expected_score()
+        
+        
 
         free_positions = list(get.free_positions())
 
@@ -232,7 +265,8 @@ if __name__ == "__main__":
             # How many digits should we try on recursion?
             # TODO: Better logic here, or try everything
 
-            digits_try_to = random.choices(new_get.digits, k=max(3 - level, 1))
+            digits_try_to = random.choices(new_get.digits, k=max(5 - level, 1))
+            digits_try_to = list(new_get.digits)
 
             prob = 1 / len(digits_try_to)
 
@@ -250,7 +284,7 @@ if __name__ == "__main__":
         return min(results.items(), key=operator.itemgetter(1))
 
     # Create some random games
-    for seed in range(10):
+    for seed in range(1):
 
         st = time.time()
 
@@ -270,8 +304,11 @@ if __name__ == "__main__":
         get = Get.get1000()  # Get.get1000()
         for digit in nature:
 
-            opt_index, opt_score = get_best_placement(get, digit, ell=2)
+            #opt_index, opt_score = get_best_placement(get, digit, ell=2)
             # print(f"Got digit {digit}. Placing it in {index} for expected score: {opt_score}")
+            #get = get.set_item(opt_index, digit)
+            
+            opt_index = random.choice(list(get.free_positions()))
             get = get.set_item(opt_index, digit)
 
             # print(get)
@@ -280,3 +317,227 @@ if __name__ == "__main__":
         print(f"Time spent: {time.time() - st}")
 
         print()
+        
+        
+        
+if __name__ == "__main__":
+
+
+    # =============================================================================
+    # ============ THE STRATEGY IS FIXED, TRY EVERY POSSIBLE DICE ROLL ============
+    # =============================================================================
+
+    import functools
+
+    def get_best_placement(get, digit:int, level=0):
+        """Given a GET instance and a digit, returns (best_index, expected_score)."""
+        assert digit in get.digits
+    
+        free_positions = list(get.free_positions())
+    
+        # CASE 1: Bottom of recursion - there is only one placement available.
+        if len(free_positions) == 1:
+            index = free_positions[0]
+            return index, get.set_item(index, digit).score()
+    
+        # CASE: There are many placements available, try each of them.
+        results = {}
+        for index in get.canonical_free_positions():
+    
+            # Set this digit in the free position
+            new_get = get.set_item(index, digit)
+    
+            # Given that the digit was placed in the position and nature yields
+            # a new digit, what are the expected scores we can get?
+            indices_and_scores = (
+                get_best_placement(new_get, digit, level=level + 1) for digit in new_get.digits
+            )
+            scores = (score for (index, score) in indices_and_scores)
+    
+            # Here we take E[|x_i|**(ell)]**(1/ell) - an expected value l norm
+            # The case ell = 1 corresponds to the expected value
+            # The case ell -> infty corresponds to minimax
+            ell = 1 # Set to ell >= 1
+            prob = 1 / len(new_get.digits)
+            results[index] = (sum(abs(s) ** ell * prob for s in scores)) ** (1 / ell)
+    
+        return min(results.items(), key=operator.itemgetter(1))
+    
+    algo_scores, random_scores = [], []
+    import collections
+    results = collections.defaultdict(int)
+
+    # Create some random games
+    import tqdm
+    
+    for seed in tqdm.tqdm(range(1000)):
+
+        st = time.time()
+
+        random.seed(seed)
+        get = Get.get42()  # Get.get1000()
+        nature = [random.choice(get.digits) for _ in range(get.shape[0] * get.shape[1])]
+
+        for digit in nature:
+            opt_index, opt_score = get_best_placement(get, digit)
+            get = get.set_item(opt_index, digit)
+
+        # print(f"Final score using algo: {get.score()}")
+        algo_scores.append(get.score() + get.target)
+        
+        random.seed(seed)
+        get2 = Get.get42()  # Get.get1000()
+        nature = [random.choice(get.digits) for _ in range(get.shape[0] * get.shape[1])]
+
+        for digit in nature:
+            opt_index = random.choice(list(get2.free_positions()))
+            get2 = get2.set_item(opt_index, digit)
+
+        # print(f"Final score using random: {get.score()}")
+        random_scores.append(get2.score() + get.target)
+        
+        if abs(get.score()) < abs(get2.score()):
+            results["algo"] += 1
+        elif abs(get.score()) > abs(get2.score()):
+            results["random"] += 1
+        else:
+            results["draw"] += 1
+        
+        
+    import matplotlib.pyplot as plt
+    import collections
+    import numpy as np
+    
+    data_to_plot = collections.Counter(algo_scores)
+    x = np.arange(0, max(data_to_plot.keys()) + 1)
+    y = np.zeros_like(x)
+    for i, j in data_to_plot.items():
+        y[i] = j
+        
+    plt.bar(x, y, label="Algorithm", alpha=0.5, zorder=15)
+    
+    
+    data_to_plot = collections.Counter(random_scores)
+    x = np.arange(0, max(data_to_plot.keys()) + 1)
+    y = np.zeros_like(x)
+    for i, j in data_to_plot.items():
+        y[i] = j
+        
+    plt.bar(x, y, label="Random", alpha=0.5, zorder=15)
+    
+    plt.axvline(x=42, label="42", color="red", ls="--", zorder=25)
+
+    plt.legend()
+    plt.show()
+    
+    
+    
+if __name__ == "__main__":
+
+
+    # =============================================================================
+    # ============ THE STRATEGY IS FIXED, TRY EVERY POSSIBLE DICE ROLL ============
+    # =============================================================================
+
+    import functools
+
+    def get_best_placement(get, digit:int, level=0, ell=1):
+        """Given a GET instance and a digit, returns (best_index, expected_score)."""
+        assert digit in get.digits
+    
+        free_positions = list(get.free_positions())
+    
+        # CASE 1: Bottom of recursion - there is only one placement available.
+        if len(free_positions) == 1:
+            index = free_positions[0]
+            return index, get.set_item(index, digit).score()
+    
+        # CASE: There are many placements available, try each of them.
+        results = {}
+        for index in get.canonical_free_positions():
+    
+            # Set this digit in the free position
+            new_get = get.set_item(index, digit)
+    
+            # Given that the digit was placed in the position and nature yields
+            # a new digit, what are the expected scores we can get?
+            indices_and_scores = (
+                get_best_placement(new_get, digit, level=level + 1, ell=ell) for digit in new_get.digits
+            )
+            scores = (score for (index, score) in indices_and_scores)
+    
+            # Here we take E[|x_i|**(ell)]**(1/ell) - an expected value l norm
+            # The case ell = 1 corresponds to the expected value
+            # The case ell -> infty corresponds to minimax
+            prob = 1 / len(new_get.digits)
+            results[index] = (sum(abs(s) ** ell * prob for s in scores)) ** (1 / ell)
+    
+        return min(results.items(), key=operator.itemgetter(1))
+    
+    algo_scores, random_scores = [], []
+    import collections
+    results = collections.defaultdict(int)
+
+    # Create some random games
+    import tqdm
+    
+    for seed in tqdm.tqdm(range(10000)):
+
+        st = time.time()
+
+        random.seed(seed)
+        get = Get.get42()  # Get.get1000()
+        nature = [random.choice(get.digits) for _ in range(get.shape[0] * get.shape[1])]
+
+        for digit in nature:
+            opt_index, opt_score = get_best_placement(get, digit, ell=1)
+            get = get.set_item(opt_index, digit)
+
+        # print(f"Final score using algo: {get.score()}")
+        algo_scores.append(get.score() + get.target)
+        
+        random.seed(seed)
+        get2 = Get.get42()  # Get.get1000()
+        nature = [random.choice(get.digits) for _ in range(get.shape[0] * get.shape[1])]
+
+        for digit in nature:
+            opt_index, opt_score = get_best_placement(get2, digit, ell=10)
+            get2 = get2.set_item(opt_index, digit)
+
+        # print(f"Final score using random: {get.score()}")
+        random_scores.append(get2.score() + get.target)
+        
+        if abs(get.score()) < abs(get2.score()):
+            results["algo"] += 1
+        elif abs(get.score()) > abs(get2.score()):
+            results["random"] += 1
+        else:
+            results["draw"] += 1
+        
+        
+    import matplotlib.pyplot as plt
+    import collections
+    import numpy as np
+    
+    data_to_plot = collections.Counter(algo_scores)
+    x = np.arange(0, max(data_to_plot.keys()) + 1)
+    y = np.zeros_like(x)
+    for i, j in data_to_plot.items():
+        y[i] = j
+        
+    plt.bar(x, y, label="ell = 1", alpha=0.5, zorder=15)
+    
+    
+    data_to_plot = collections.Counter(random_scores)
+    x = np.arange(0, max(data_to_plot.keys()) + 1)
+    y = np.zeros_like(x)
+    for i, j in data_to_plot.items():
+        y[i] = j
+        
+    plt.bar(x, y, label="high ELL", alpha=0.5, zorder=15)
+    
+    plt.axvline(x=42, label="42", color="red", ls="--", zorder=25)
+
+    plt.legend()
+    plt.show()
+
